a37ef645ee5b0084a85b25bc716d60951be355bd
Enze Wang <enzewang@amazon.com>
2018-07-27 16:55:23 +0800

    Ignore test

--- src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
+++ src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
@@ -1,13 +1,10 @@
 package com.wez.experimental;
 
 import org.junit.Ignore;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class FacePlusPlusClientTest {
 
-    @Test
+    @Ignore
     public void upload() {
         FacePlusPlusResponse response = new FacePlusPlusClient().upload("/Users/enzewang/Downloads/human.jpg");
         System.out.println(response);
