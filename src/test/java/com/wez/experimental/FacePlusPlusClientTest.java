cd4d27e1aa371cec8f1ea21d3c944e5cd73fb114
Enze Wang <enzewang@amazon.com>
2018-07-27 17:15:51 +0800

    Add test case

--- src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
+++ src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
@@ -7,6 +7,27 @@ public class FacePlusPlusClientTest {
     @Ignore
     public void upload() {
         FacePlusPlusResponse response = new FacePlusPlusClient().upload("/Users/enzewang/Downloads/human.jpg");
-        System.out.println(response);
+        if (response.getHumanbodies() != null) {
+            System.out.println(response.getHumanbodies().size() + " human(s) identified");
+            for (int i = 0; i < response.getHumanbodies().size(); i++) {
+                FacePlusPlusResponse.HumanBody humanBody = response.getHumanbodies().get(i);
+                System.out.println("Human " + (i + 1) + ":");
+                System.out.println("\tConfidence: " + humanBody.getConfidence());
+                FacePlusPlusResponse.Attributes attributes = humanBody.getAttributes();
+                if (attributes != null) {
+                    FacePlusPlusResponse.Gender gender = attributes.getGender();
+                    String possibleGender = gender.getMale() > gender.getFemale() ? "Male" : "Female";
+                    System.out.println("\tGender: " + possibleGender);
+                    FacePlusPlusResponse.UpperBodyCloth upper_body_cloth = attributes.getUpper_body_cloth();
+                    if (upper_body_cloth != null) {
+                        System.out.println("\tUpper cloth color: " + upper_body_cloth.getUpper_body_cloth_color());
+                    }
+                    FacePlusPlusResponse.LowerBodyCloth lower_body_cloth = attributes.getLower_body_cloth();
+                    if (lower_body_cloth != null) {
+                        System.out.println("\tLower cloth color: " + lower_body_cloth.getLower_body_cloth_color());
+                    }
+                }
+            }
+        }
     }
 }
