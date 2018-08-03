790e741e7d027b6355cd83f7e9a4cf0841a0f956
Enze Wang <enzewang@amazon.com>
2018-08-03 10:14:30 +0800

    Add API keys as params

--- src/main/java/com/wez/experimental/FacePlusPlusClient.java
+++ src/main/java/com/wez/experimental/FacePlusPlusClient.java
@@ -1,5 +1,6 @@
 package com.wez.experimental;
 
+import lombok.AllArgsConstructor;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -8,6 +9,7 @@ import org.apache.http.entity.mime.content.StringBody;
 
 import java.io.File;
 
+@AllArgsConstructor
 public class FacePlusPlusClient extends AbstractUploadClient<FacePlusPlusResponse> {
 
     private static final String UPLOAD_URL = "https://api-cn.faceplusplus.com/humanbodypp/v1/detect";
@@ -15,12 +17,19 @@ public class FacePlusPlusClient extends AbstractUploadClient<FacePlusPlusRespons
     private static final String API_SECRET = "PHHlBNyqIqEvxseV77di6-UveP7HMNQq";
     private static final String ATTRIBUTES = "gender,upper_body_cloth,lower_body_cloth";
 
+    private final String apiKey;
+    private final String apiSecret;
+
+    public FacePlusPlusClient() {
+        this(API_KEY, API_SECRET);
+    }
+
     @Override
     protected HttpPost buildRequest(String jpgFilePath) {
         HttpPost post = new HttpPost(UPLOAD_URL);
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        builder.addPart("api_key", new StringBody(API_KEY, ContentType.MULTIPART_FORM_DATA));
-        builder.addPart("api_secret", new StringBody(API_SECRET, ContentType.MULTIPART_FORM_DATA));
+        builder.addPart("api_key", new StringBody(apiKey, ContentType.MULTIPART_FORM_DATA));
+        builder.addPart("api_secret", new StringBody(apiSecret, ContentType.MULTIPART_FORM_DATA));
         builder.addPart("return_attributes", new StringBody(ATTRIBUTES, ContentType.MULTIPART_FORM_DATA));
         builder.addBinaryBody("image_file", new File(jpgFilePath), ContentType.IMAGE_JPEG, "file.jpg");
         HttpEntity multipart = builder.build();
--- src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
+++ src/test/java/com/wez/experimental/FacePlusPlusClientTest.java
@@ -3,7 +3,6 @@ package com.wez.experimental;
 import org.junit.Ignore;
 
 public class FacePlusPlusClientTest {
-
     @Ignore
     public void upload() {
         FacePlusPlusResponse response = new FacePlusPlusClient().upload("/Users/enzewang/Downloads/human.jpg");
